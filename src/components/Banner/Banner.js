a3ae35ec3f7bb0dfaf0246f52a09ac6fac2e6255
ROHITH <aorohith@gmail.com>
2021-11-07 13:28:42 +0530

    Banner component finsidhed as dynamic

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
 import "./Banner.css"
+import axios from '../../axios'
+import {API_KEY,imageUrl} from '../../constants/constants'
 
 function Banner() {
+    const [movie, setMovie] = useState({})
+    useEffect(() => {
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            console.log(response.data.results[0]);
+            setMovie(response.data.results[1]);
+        })
+    }, [])
     return (
-        <div className="banner">
+        
+        <div
+        style = {{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ''})`}}
+        className="banner">
             <div className="content">
-                <h1 className="title">Movie Name</h1>
+                <h1 className="title">{movie ? movie.title : ''}</h1>
                 <div className="banner_buttons">
                     <button className="button">Play</button>
                     <button className="button">My List</button>
                 </div>
-                <h1 className="discription">Lorem Ipsum is simply dummy text of the printing and typesetting survived not only five centuries</h1>
+                <h1 className="discription">{movie ? movie.overview : ''}</h1>
             </div>
             <div className="fade-bottom"></div>
         </div>
