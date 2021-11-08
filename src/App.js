01fb154360f58c074f03df2c21e947a1562f448e
ROHITH <aorohith@gmail.com>
2021-11-08 12:57:48 +0530

    Design bug fixed, Category added and gh-pages library configured

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavBar from './Components/NavBar/NavBar' 
 import React from 'react'
-import {originals,action} from './urls'
+import {originals,action,comedy,horror,romance,documentaries} from './urls'
 import "./App.css"
 import Banner from './Components/Banner/Banner'
 import RowPost from './Components/RowPost/RowPost'
@@ -12,6 +12,10 @@ function App() {
       <Banner/>
       <RowPost url={originals} title='Netflix Originals' />
       <RowPost url={action} title='Action' isSmall />
+      <RowPost url={comedy} title='ComedyMovies' isSmall />
+      <RowPost url={horror} title='HorrorMovies' isSmall />
+      <RowPost url={romance} title='RomanceMovies' isSmall />
+      <RowPost url={documentaries} title='Documentaries' isSmall />
     </div>
   )
 }
